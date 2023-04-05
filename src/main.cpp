94108607ff02e3c643ac38c1ee9eddc86df75d89
Ilia <iliyaghorbanianb@gmail.com>
2023-04-05 04:45:50 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+
+#include <vulkan/vulkan_funcs.hpp>
+
+SDL_Window *window;
+char* window_name = "example SDL2 Vulkan application";
+
