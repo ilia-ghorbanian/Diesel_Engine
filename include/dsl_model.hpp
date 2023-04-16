81b3eaaf4bbde57fe0e309bd41d8175522c1a034
Ilia <iliyaghorbanianb@gmail.com>
2023-04-15 23:07:48 -0400

    added model 2d model support and fixed the CMakeLists. Also included GLM in the project

---
+++ include/dsl_model.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "dsl_device.hpp"
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+
+namespace dsl{
+    class DslModel {
+        public:
+
+
+        struct Vertex{
+            glm::vec2 position;
+
+            static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+            static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+        };
+
+        DslModel(DslDevice &device_, const std::vector<Vertex> &vertices);
+        ~DslModel();
+
+        DslModel(const DslModel &) = delete;
+        DslModel &operator=(const DslModel &) = delete;
+
+
+        void bind(VkCommandBuffer commandBuffer);
+        void draw(VkCommandBuffer commandBuffer);
+
+        
+        private:
+        void createVertexBuffers(const std::vector<Vertex> &vertices);
+
+        DslDevice &dslDevice;
+        VkBuffer vertexBuffer;
+        VkDeviceMemory vertexBufferMemory;
+        uint32_t vertexCount;
+
+
+    };
+}
---
+++ src/dsl_model.cpp
@@ -0,0 +1,68 @@
+#include "dsl_model.hpp"
+#include <cassert>
+namespace dsl{
+    
+    
+    DslModel::DslModel(DslDevice &device, const std::vector<Vertex> &vertices) : dslDevice{device} {
+        createVertexBuffers(vertices);
+    }
+    
+    
+    DslModel::~DslModel(){
+        vkDestroyBuffer(dslDevice.device(), vertexBuffer, nullptr);
+        vkFreeMemory(dslDevice.device(), vertexBufferMemory, nullptr);
+    }
+
+
+    void DslModel::createVertexBuffers(const std::vector<Vertex> &vertices){
+        vertexCount = static_cast<uint32_t>(vertices.size());
+        assert(vertexCount >= 3 && "Vertex count must be at least 3");
+        VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+        dslDevice.createBuffer(bufferSize,
+         VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, 
+         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+         vertexBuffer,
+         vertexBufferMemory);
+
+        void *data;
+        vkMapMemory(dslDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(dslDevice.device(), vertexBufferMemory);
+
+    }
+
+
+    void DslModel::bind(VkCommandBuffer commandBuffer){
+        VkBuffer buffers[] = {vertexBuffer};
+        VkDeviceSize offsets[] = {0};
+        vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+    }
+
+    void DslModel::draw(VkCommandBuffer commandBuffer){
+        vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+    }
+
+
+
+
+    std::vector<VkVertexInputBindingDescription> DslModel::Vertex::getBindingDescriptions(){
+        std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
+        bindingDescriptions[0].binding = 0;
+        bindingDescriptions[0].stride = sizeof(Vertex);
+        bindingDescriptions[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return bindingDescriptions;
+    }
+
+
+    std::vector<VkVertexInputAttributeDescription> DslModel::Vertex::getAttributeDescriptions(){
+        std::vector<VkVertexInputAttributeDescription> attributeDescription(1);
+        attributeDescription[0].binding = 0;
+        attributeDescription[0].location = 0;
+        attributeDescription[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescription[0].offset = 0;
+        return attributeDescription;
+    }
+
+
+}
