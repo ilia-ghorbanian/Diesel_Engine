4f1adc1d04fe3f2fb4ccf86f28cc7ff8eedd0baf
Ilia <iliyaghorbanianb@gmail.com>
2023-04-15 23:28:51 -0400

    multicolor triangle!

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -66,9 +66,9 @@ namespace dsl {
     void FirstApp::loadModels() {
 
         std::vector<DslModel::Vertex>  vertices {
-            {{0.0f, -0.5f}},
-            {{0.5f, 0.5f}},
-            {{-0.5f, 0.5f}}
+            {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+            {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
         };
         // sierpinski(vertices, 5, {-0.5f, 0.5f}, {0.5f, 0.5f}, {0.0f, -0.5f});
 
